14fac17f95b17d1fff9eee3732a85d4711312354
Gluegun <kirja5@mail.ru>
2020-04-21 09:56:33 +0300

    new writer to file added

--- src/main/java/HomeWork/Main.java
+++ src/main/java/HomeWork/Main.java
@@ -5,25 +5,53 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.StandardOpenOption;
-import java.util.Set;
+import java.util.*;
 import java.util.concurrent.ForkJoinPool;
 
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         long start = System.currentTimeMillis();
-        String url = "https://skillbox.ru";
+        String url = "https://skillbox.ru/";
         Links links = new Links(url);
         Set<String> invoke = new ForkJoinPool().invoke(links);
 
         System.out.println(invoke.size());
         System.out.println("Время работы программы: " + (System.currentTimeMillis() - start) + " мс.");
 
-
         Path filePath = Paths.get("src/main/resources/siteMap.txt");
 
+        writer(invoke,filePath);
+
+    }
+
+    public static void writer(Set<String> strings, Path filePath) {
+        List<String> links = new ArrayList<>(strings);
+        links.sort(Comparator.naturalOrder());
+
+        List<String> newStrings = new LinkedList<>();
+
+        for (String string : links) {
+            StringBuilder builder = new StringBuilder();
+            string = string.substring(string.indexOf("skill"));
+            String[] split = string.split("/");
+
+            for (int i = 0; i < split.length; i++) {
+                builder.append("\t");
+            }
+            builder.append(string);
+            builder.insert(builder.indexOf("s"), "https://");
+            newStrings.add(builder.toString());
+        }
+
+        for (int i = 0; i < newStrings.size(); i++) {
+            String string = newStrings.get(i);
+            string = string.substring(1);
+            newStrings.set(i, string);
+        }
+
         try {
-            Files.write(filePath, invoke, StandardOpenOption.WRITE, StandardOpenOption.CREATE);
+            Files.write(filePath, newStrings, StandardOpenOption.WRITE, StandardOpenOption.CREATE);
         } catch (IOException e) {
             e.printStackTrace();
         }
